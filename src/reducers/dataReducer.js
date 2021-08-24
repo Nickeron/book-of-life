4fcb350ae3c79d57c08b17383d4fb854abaf3b59
Nick Karamousadakis <nkaramousadakis@outlook.com>
2021-08-24 14:51:47 +0300

    adds Reducer for data actions

---
+++ src/reducers/dataReducer.js
@@ -0,0 +1,21 @@
+import { IMPORT_IMAGE,	DELETE_IMAGE, EDIT_TEXT, FETCH_QUIZ_DATA, FETCH_EXP_DATA} from '../actions/types';
+import _ from 'lodash';
+
+const dataReducer = (state = {}, action) =>
+{
+	switch( action.type) 
+	{
+		case FETCH_EXP_DATA:
+		case FETCH_QUIZ_DATA:
+			return {...state, ..._.mapKeys(action.payload, 'id')};
+		case IMPORT_IMAGE:
+		case EDIT_TEXT:
+			return {...state, [action.payload.id]: action.payload};
+		case DELETE_IMAGE:
+			return _.omit(state, action.payload);
+		default:
+			return state;		
+	}
+}
+
+export default dataReducer;
